0109aea4dd47c17f45716e354a6d800139b285f2
zhaoyuhang <zhaoyuhang@users.noreply.github.com>
2014-11-15 20:29:57 +0800

    Update Category.cs

--- wuw_v0.1/code/WUW01/classes/Category.cs
+++ wuw_v0.1/code/WUW01/classes/Category.cs
@@ -1,16 +1,26 @@
-using System;
-using System.Collections;
+using System;//string
+using System.Collections;//arraylist
 using System.Collections.Generic;
 using System.Text;
-using System.Diagnostics;
-
-namespace WUW01
+using System.Diagnostics;//using允许在命名空间中使用类型,
+namespace WUW01//name space声明命名空间的名称，并使跟在声明后面的源代码将在该命名空间中进行编译。
 {
     public class Category
     {
-        private string      _name;
-        private ArrayList   _prototypes;
-
+        private string      _name;//文本即unicode,字符串
+        private ArrayList   _prototypes;//AL使用大小会根据需要动态增加的数组？？
+/*
+public  
+访问不受限制。  
+protected  
+访问仅限于包含类或从包含类派生的类型。  
+internal  
+访问仅限于当前程序集。  
+protected  internal  
+访问仅限于从包含类派生的当前程序集或类型。  
+private  
+访问仅限于包含类型。 
+*/
         public Category(string name)
         {
             _name = name;
@@ -20,7 +30,7 @@ namespace WUW01
         public Category(string name, Gesture firstExample)
         {
             _name = name;
-            _prototypes = new ArrayList();
+            _prototypes = new ArrayList();//初始化 ArrayList 类的新实例，该实例为空并且具有默认初始容量
             AddExample(firstExample);
         }
         
@@ -41,7 +51,7 @@ namespace WUW01
             {
                 return _name;
             }
-        }
+        }//获取名字
 
         public int NumExamples
         {
@@ -49,7 +59,7 @@ namespace WUW01
             {
                 return _prototypes.Count;
             }
-        }
+        }//获取原型数
 
         /// <summary>
         /// Indexer that returns the prototype at the given index within
@@ -70,19 +80,19 @@ namespace WUW01
                     return null;
                 }
             }
-        }
+        }//对应种类
 
         public void AddExample(Gesture p)
         {
             bool success = true;
             try
             {
-                // first, ensure that p's name is right
+                // first, ensure that p's name is right首先保证手势的名称正确
                 string name = ParseName(p.Name);
                 if (name != _name)
                     throw new ArgumentException("Prototype name does not equal the name of the category to which it was added.");
 
-                // second, ensure that it doesn't already exist
+                // second, ensure that it doesn't already exist第二保证它没有已经存在
                 for (int i = 0; i < _prototypes.Count; i++)
                 {
                     Gesture p0 = (Gesture) _prototypes[i];
@@ -90,7 +100,7 @@ namespace WUW01
                         throw new ArgumentException("Prototype name was added more than once to its category.");
                 }
             }
-            catch (ArgumentException ex)
+            catch (ArgumentException ex)//异常处理
             {
                 Console.WriteLine(ex.Message);
                 success = false;
